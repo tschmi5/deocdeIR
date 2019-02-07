e94ba034d34dc2b0652711f3cc77b1de869c4fc3
Tyler Schmitz <1timetyler@gmail.com>
2019-02-07 16:40:47 -0500

    Identified Key Locations

--- src/KeyArray.java
+++ src/KeyArray.java
@@ -4,19 +4,22 @@
 public class KeyArray {
     private static KeyArray instance;
     private Key[] keys;
+    private int index;
 
     private KeyArray(int rows,int cols){
         this.keys = new Key[rows];
         for(int i = 0; i < keys.length; i++){
             keys[i] = new Key(cols);
-            System.out.println("Key Array initialized");
         }
-
+        System.out.println("Key Array initialized");
     }
     public static synchronized KeyArray getInstance(int rows,int cols){
         if(instance == null){
             instance = new KeyArray(rows,cols);
         }
         return instance;
+    }
+    public void setKey(){
+
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -10,10 +10,21 @@ public class Main {
             final int COLS = 67;
             final int ROWS = 24;
             KeyArray ka = KeyArray.getInstance(COLS,ROWS);
-            Scanner file = new Scanner(new File("data.txt"));
+            Scanner file = new Scanner(new File("C:\\develop\\git-source\\decodeIR\\src\\data.txt"));
+
+
+            while(file.hasNextLine()){
+                String token = file.nextLine();
+                Scanner in = new Scanner(token);
+                if(in.findInLine("name KEY_") != null){
+                    String name = in.next();
+                    System.out.println(name);
+                }
+
+            }
 
         } catch (FileNotFoundException e) {
-
+            System.err.println("Error, quitting");
         }
     }
 
